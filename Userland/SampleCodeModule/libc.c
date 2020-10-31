7a11e84b10d375819e0c8f99615a74b1b0e433a5
Juan Oriana <joriana@itba.edu.ar>
2020-10-31 16:32:49 -0300

    NO ANDA SCANF

--- Userland/SampleCodeModule/libc.c
+++ Userland/SampleCodeModule/libc.c
@@ -7,11 +7,14 @@
 // WRAPPEO DE FUNCIONES DE ASM Y ALGUNAS PRECISIONES PARA USO DE USUARIO
 
 
+char buffer[256]= {0};
+
 int print(char *str, ...)
 {
     va_list vl;
     int i = 0, j = 0;  // i lectura en str  - j pos en buffer
     char buff[100] = {0}, tmp[20];
+    char *str_arg;
     va_start(vl, str);
     while (str && str[i])
     {
@@ -33,6 +36,13 @@ int print(char *str, ...)
                     j += strlen(tmp);
                     break;
                 }
+                case 's':
+                {
+                    str_arg = va_arg(vl, char *);
+                    strcpy(&buff[j], str_arg);
+                    j += strlen(str_arg);
+                    break;
+                }
             }
         }
         else
@@ -48,49 +58,118 @@ int print(char *str, ...)
 }
 
 
-int printErr(char *buff, int length){
-    return write(2, buff, length);
-}
-
 int putChar(char c){
     char buff[2] = {0};
     buff[0] = c;
     return write(1,buff,2);
 }
 
-int scan(char* buff, int length){
-    return read(0,buff,length);
-}
-
-char getChar(){
-    char buff[2] = {0};
-    return read(0, buff, 2);
-    return buff[0];
-}
-
-int strlen(const char *s)
+int scan(const char *format, ...)
 {
-    int i;
-    for (i = 0; s[i] != '\0'; i++);
-    return i;
-}
+    va_list args;
+    va_start(args, format);
 
+    readLine(buffer);
+    int buffIdx = 0;
+    int fmtIdx = 0;
 
-char *strcpy(char *destination, const char *source)
-{
-    
-    if (destination == (void *)0)
-        return destination;
+    int result = 0;
+    int flag = 0;
 
-    char *ptr = destination;
+    int auxIdx = 0;
+    char *auxStr;
+    int *auxNum;
 
-    while (*source != '\0')
+    while (format[fmtIdx] != '\0' && buffer[buffIdx] != '\0' && !flag)
     {
-        *destination = *source;
-        destination++;
-        source++;
+        if (format[fmtIdx] != '%')
+        {
+            if (format[fmtIdx] != buffer[buffIdx])
+            {
+                flag = -1;
+            }
+            else
+            {
+                fmtIdx++;
+                buffIdx++;
+            }
+        }
+        else
+        {
+            fmtIdx++;
+            switch (format[fmtIdx])
+            {
+            case 'd':
+            case 'i':
+                *(int *)va_arg(args, int *) = itoa(&buff[j], &out_loc, 10);
+                buffIdx += out_loc - &buffer[buffIdx];
+                fmtIdx++;
+                break;
+            case 'c':
+                *(char *)va_arg(args, char *) = buffer[j];
+                j++;
+                ret++;
+                break;
+            case 's': //String hasta espacio
+                auxStr = va_arg(args, char *);
+                auxIndex = bufferIndex;
+                while (buffer[bufferIndex] != ' ' && buffer[bufferIndex] != '\0')
+                {
+                    *auxStr = buffer[bufferIndex];
+                    auxStr++;
+                    bufferIndex++;
+                }
+                if (bufferIndex > auxIndex)
+                {
+                    auxStr[auxIndex] = '\0';
+                    result++;
+                }
+                else
+                {
+                    auxStr[auxIndex] = '\0';
+                    flag = 1;
+                }
+                formatIndex++;
+                break;
+
+            default:
+                flag = -1;
+                break;
+            }
+        }
+        return flag;
     }
 
-    *destination = '\0';
-    return ptr;
-}
+    char getChar()
+    {
+        char buff[2] = {0};
+        return read(0, buff, 2);
+        return buff[0];
+    }
+
+    int strlen(const char *s)
+    {
+        int i;
+        for (i = 0; s[i] != '\0'; i++)
+            ;
+        return i;
+    }
+
+    char *strcpy(char *destination, const char *source)
+    {
+
+        if (destination == (void *)0)
+            return destination;
+
+        char *ptr = destination;
+
+        while (*source != '\0')
+        {
+            *destination = *source;
+            destination++;
+            source++;
+        }
+
+        *destination = '\0';
+        return ptr;
+    }
