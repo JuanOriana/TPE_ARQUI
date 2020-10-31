bdb42d1048f29a84b276805fb9b03fdb71387c80
Agustin Naso <anaso@itba.edu.ar>
2020-10-31 20:40:30 -0300

    chess board v1

--- Userland/SampleCodeModule/chess/board.c
+++ Userland/SampleCodeModule/chess/board.c
@@ -1,40 +1,112 @@
+#define WHITE 1
+#define BLACK -1
+enum wpieces {WPAWN=1,WBISHOP=2,WKNIGHT=3,WROOK=4,WQUEEN=5,WKING=6};
+enum bpieces {BPAWN=-1,BBISHOP=-2,BKNIGHT=-3,BROOK=-4,BQUEEN=-5,BKING=-6};
 
-enum wpieces {WPAWN=1,WBISHOP,WKNIGHT,WROOK,WQUEEN,WKING};
-enum bpieces {BPAWN=11,BBISHOP,BKNIGHT,BROOK,BQUEEN,BKING};
+#define SIZE 8
 
-
-char board[8][8] ={
-    {BROOK,BKNIGHT,BBISHOP,BQUEEN,BKING,BBISHOP,BKNIGHT,BROOK},
+char board[SIZE][SIZE] ={
+    {BROOK,BKNIGHT,BBISHOP,BQUEEN,BKING,BBISHOP,BKNIGHT,BROOK}, //0
     {BPAWN,BPAWN,BPAWN,BPAWN,BPAWN,BPAWN,BPAWN,BPAWN,BPAWN},
     {0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0,0,0},
     {WPAWN,WPAWN,WPAWN,WPAWN,WPAWN,WPAWN,WPAWN,WPAWN,WPAWN},
-    {WROOK,WKNIGHT,WBISHOP,WQUEEN,WKING,WBISHOP,WKNIGHT,WROOK}
+    {WROOK,WKNIGHT,WBISHOP,WQUEEN,WKING,WBISHOP,WKNIGHT,WROOK}  //7
 };
 
 
-int move(int piece, int from_x, int from_y, int to_x, int to_y){
-    board[from_x][from_x] = 0;
-    board[to_x][to_y] = piece;
+int move(int fromX, int fromY, int toX, int toY){
+    board[toY][toX] = board[fromY][fromX];
+    board[fromY][fromX] = 0;
 }
 
-int checkMove(int piece, int from_x, int from_y, int to_x, int to_y){
-    switch (piece%10)
-    {
-    case 1:
-    {
-        if(1) { return -1;}
+int checkMove(int fromX, int fromY, int toX, int toY)
+{
 
-        else if(to_x == 2 + from_x){
-            if(from_x == 1 && from_y==to_y && board[to_x][to_y]==0) {
-                move(piece,from_x,from_y,to_x,to_y);
-            }
-            else return -1;
-        }
-        else {
-            if (to_y == from_y && board[to_x][to_y]==0) {move(piece,from_x,fr_y,)}
-        }
-        
+    int piece = board[fromY][fromX];
+    int side = piece / abs(piece);
+
+    switch (piece)
+    {
+        case (WPAWN):
+        case (BPAWN):
+            int flag = checkPawn(side,fromX,fromY,toX,toY);
+            if (flag)
+                move(fromX,fromY,toX,toY);
+            return flag;
+            break;
+
+        case(WROOK):
+        case(BROOK):
+            return checkRook(side,fromX,fromY,toX,toY);
+            break;
+        case(WKNIGHT):
+        case(BKNIGHT):
+            break;
+        case(WBISHOP):
+        case(BBISHOP):
+            break;
+        case(WQUEEN):
+        case(BQUEEN):
+            break;
+        case(WKING):
+        case(BKING):
+            break;
     }
+}
+int checkBounds(int x,int y){
+    if(x<0 || x>SIZE-1 || y<0 || y>SIZE-1)
+        return -1;
+    return 0;
+}
+
+int checkPawn(int side, int fromX, int fromY, int toX, int toY)
+{
+    int flag = checkBounds(toX,toY);
+    if (!flag)
+        return flag;
+    
+    int absDistY = side*(toY - fromY);
+    int absDistX = abs(toX-fromX);
+    //Chequeos de movimiento vertical
+    if (side * (absDistY) <= 0 || absDistY > 2
+        || (side == BLACK && fromY != 1 && absDistY == 2) || (side == WHITE && fromY != 6 && absDistY == 2))
+        return 0;
+
+    //Chequeos de movimiento horizontal
+    if (absDistX>1)
+        return 0;
+    if (absDistX==0)
+        return board[toY][toX] == 0;
+    return board[toY][toX]*side < 0;
+}
+
+int checkRook(int side, int fromX, int fromY, int toX, int toY ){
+    int flag = checkBounds(toX,toY);
+    if (!flag)
+        return flag;
+
+    if (fromX!=toX){
+        //si X cambia, y no puede hacerlo
+        if (fromY!=toY)
+            return 0;
+        //No puede haber piezas de fromX+1 a toX-1
+        for (int x= fromX+1; x<toX;x++)
+            if (board[toY][x]!=0)
+                return 0;
+        return side*board[toY][toX]  <= 0;
+    }
+    else
+    {
+        if (fromX != toX)
+            return 0;
+
+        //Idem en Y
+        for (int y= fromY+1; y<toY;y++)
+            if (board[y][toX]!=0)
+                return 0;
+        return side*board[toY][toX] <= 0;
+    }
+}
