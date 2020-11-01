5bc588455da8b2d77b1756901f7725376b3279ef
Agustin Naso <anaso@itba.edu.ar>
2020-11-01 02:32:36 -0300

    Movilty of pieces completed

--- Userland/SampleCodeModule/chess/board.c
+++ Userland/SampleCodeModule/chess/board.c
@@ -28,13 +28,15 @@ int checkMove(int fromX, int fromY, int toX, int toY)
     int piece = board[fromY][fromX];
     int side = piece / abs(piece);
 
+    int flag = checkBounds(toX,toY);
+    if (!flag)
+        return flag;
+
     switch (piece)
     {
         case (WPAWN):
         case (BPAWN):
-            int flag = checkPawn(side,fromX,fromY,toX,toY);
-            if (flag)
-                move(fromX,fromY,toX,toY);
+                checkPawn(side,fromX,fromY,toX,toY);
             return flag;
             break;
 
@@ -44,15 +46,19 @@ int checkMove(int fromX, int fromY, int toX, int toY)
             break;
         case(WKNIGHT):
         case(BKNIGHT):
+            return checkKnight(side,fromX,fromY,toX,toY);
             break;
         case(WBISHOP):
         case(BBISHOP):
+            return checkBishop(side,fromX,fromY,toX,toY);
             break;
         case(WQUEEN):
         case(BQUEEN):
+            return checkQueen(side,fromX,fromY,toX,toY);
             break;
         case(WKING):
         case(BKING):
+            return checkKing(side,fromX,fromY,toX,toY);
             break;
     }
 }
@@ -64,10 +70,7 @@ int checkBounds(int x,int y){
 
 int checkPawn(int side, int fromX, int fromY, int toX, int toY)
 {
-    int flag = checkBounds(toX,toY);
-    if (!flag)
-        return flag;
-    
+
     int absDistY = side*(toY - fromY);
     int absDistX = abs(toX-fromX);
     //Chequeos de movimiento vertical
@@ -84,19 +87,24 @@ int checkPawn(int side, int fromX, int fromY, int toX, int toY)
 }
 
 int checkRook(int side, int fromX, int fromY, int toX, int toY ){
-    int flag = checkBounds(toX,toY);
-    if (!flag)
-        return flag;
-
     if (fromX!=toX){
         //si X cambia, y no puede hacerlo
         if (fromY!=toY)
             return 0;
-        //No puede haber piezas de fromX+1 a toX-1
-        for (int x= fromX+1; x<toX;x++)
-            if (board[toY][x]!=0)
-                return 0;
-        return side*board[toY][toX]  <= 0;
+
+        //Se mueve hacia la derecha?
+        if(fromX < toX){
+            //No puede haber piezas de fromX+1 a toX-1
+            for (int x= fromX+1; x<=toX;x++)
+                if (board[toY][x]!=0)
+                    return 0;
+            return side*board[toY][toX]  <= 0;
+        }
+        else for(int x = fromX - 1; x >=toX ; x--){
+                if (board[toY][x]!=0)
+                    return 0;
+            return side*board[toY][toX]  <= 0;
+        }
     }
     else
     {
@@ -104,11 +112,82 @@ int checkRook(int side, int fromX, int fromY, int toX, int toY ){
             return 0;
 
         //Idem en Y
-        for (int y= fromY+1; y<toY;y++)
-            if (board[y][toX]!=0)
-                return 0;
+        if(fromY < toX){
+            for (int y= fromY+1; y<=toY;y++)
+                if (board[y][toX]!=0)
+                    return 0;
+            return side*board[toY][toX] <= 0;
+        }
+        else {
+             for (int y= fromY-1; y>=toY;y--)
+                if (board[y][toX]!=0)
+                    return 0;
+            return side*board[toY][toX] <= 0;
+        }
+    }
+}
+
+int checkKnight(int side, int fromX, int fromY, int toX, int toY ){
+    int absDisX = abs(fromX - toX);
+    int absDisY = abs(fromY - toY);
+    //Chequeo 2 casilleros vertical y 1 horizontal o 2 horizontal y 1 vertical
+    if((absDisX == 1 && absDisY == 2) || (absDisX == 2 && absDisY == 1)) {
+        return side*board[toY][toX] <= 0;
+     }
+    return 0;
+}
+
+int checkBishop(int side, int fromX, int fromY, int toX, int toY ){
+    int absDisX = abs(fromX - toX);
+    int absDisY = abs(fromY - toY);
+    if(absDisX == 0 || absDisY == 0) { return 0;}
+    //Tengo 4 casos, hacia esquina inf izq, inf der, sup izq, sup der
+    //Caso 1; hacia sup der
+    if(fromX < toX && fromY < toY){
+        for(int i  =  fromX + 1; i <= toX; i++){
+            if(board[i][i] != 0) { return 0; }
+        }
+        return side*board[toY][toX] <= 0;
+    }
+    //Caso 2 : hacia inf izq
+    if(fromX > toX && fromY > toY){
+        for(int i  =  fromX -1 ; i >= toX; i--){
+            if(board[i][i] != 0) { return 0; }
+        }
+        return side*board[toY][toX] <= 0;
+    }
+    //Caso3 : hacia sup izq
+    if(fromX > toX && fromY < toY){
+        for(int i  =  fromX -1 ; i >= toX; i--){
+            if(board[i][toY - i] != 0) { return 0; }
+        }
+        return side*board[toY][toX] <= 0;
+    }
+    //Caso 4 : hacia inf der
+    if(fromX < toX && fromY > toY){
+        for(int i  =  fromX + 1 ; i <= toX; i++){
+            if(board[i][toY - i] != 0) { return 0; }
+        }
         return side*board[toY][toX] <= 0;
     }
 }
 
-//TODO : EL RESTO DE LOS MOVS
+int checkKing(int side, int fromX, int fromY, int toX, int toY){
+    int absDisX = abs(fromX - toX);
+    int absDisY = abs(fromY - toY);
+    if(absDisX <= 1  && absDisY <=1 && !(absDisX ==0 && absDisX==0)) { return side*board[toY][toX] <= 0;}
+    return 0;
+}
+
+int checkQueen(int side, int fromX, int fromY, int toX, int toY){
+    int absDisX = abs(fromX - toX);
+    int absDisY = abs(fromY - toY);
+    //El movimiento es recto?
+    if(absDisX == 0 || absDisY == 0){
+        return checkRook(side,fromX,fromY,toX,toY);
+    }
+    //Caso contrario es diagonal
+    return checkBishop(side,fromX,fromY,toX,toY);
+}
+
+
