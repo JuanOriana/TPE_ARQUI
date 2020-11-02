23a8876924c5c7a5b1c83d9291cc760ad3db902c
Agustin Naso <anaso@itba.edu.ar>
2020-11-01 22:51:27 -0300

    documentacion

--- Userland/SampleCodeModule/include/libc.h
+++ Userland/SampleCodeModule/include/libc.h
@@ -9,19 +9,42 @@
 //IN: String, Parametros variables (formatos)
 //OUT: Longitud del string
 int print(char *str, ...) ;
-
+//IN: Char a imprimir
+//OUT: 0 en caso de error, 1 caso contrario
 int putChar(char c);
+//IN: String con formato, parametros variables (formatos)
+//OUT: Longitud del string
 int scan(const char *format, ...);
+//IN:void
+//OUT: Char leido de teclado
 int getChar();
+//IN: void
+//OUT: Size del buffer leido
 int readLn();
+//IN: String
+//OUT: Longitud del string
 int strlen(const char *s);
+//IN: Buffer destino, String origen
+//OUT: Referencia al buffer destino con el source copiado
 char *strcpy(char *destination, const char *source);
+//IN: Buffer destino, String origen
+//OUT: Copia en destino hasta el primer espacio en el string source
 char *strcpyTilSpace(char *destination, const char *source);
+//IN: String que representa un numero, longitud del string
+//OUT: String convertido en integer
 int strToInt(char *str, int *size);
+//IN: Integer, buffer, base numerica a convertir
+//OUT: referencia al buffer
 char *intToStr(int num, char *str, int base);
 void clearBuff();
+//IN: Integer
+//OUT: Valor absoluto del integer
 int abs(int n);
+//IN: String, String
+//OUT: 0 si son iguales, >0 si String1 > String2, <0 caso contrario
 int strcmp(const char *str1, const char *str2);
+//IN: Numero a convertir, buffer a guardar el numero, cuantos bytes ocupa
+//OUT: Referencia al buffer con el numero guardado
 char *intToHexa(long int num, char *str, int bytes);
 
 #endif
